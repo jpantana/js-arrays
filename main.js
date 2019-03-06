2f137bd2ee4fa6aba4a5731587d92112a7da3e0a
JPantana <joshpantana@me.com>
2019-03-05 19:29:44 -0600

    added init function and dinoType for loop

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
-console.log('So... as I was saying');
+const dinosaurs = [
+    {
+        dinoType: `Brachiosaurus`,
+    },
+    {
+        dinoType: `Velociraptor`,
+    },
+    {
+        dinoType: `Stegosaurus`,
+    },
+];
+
+
+// loop over the dinosaurs
+// build up a DOM string
+// printToDom
+
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById('dino-barn');
+  selectedDiv.innerHTML = textToPrint;
+};
+
+const buildDinosaurs = () => {
+    let domString = '';
+    for(let i = 0; i < dinosaurs.length; i++ ) {
+        domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+    }
+    printToDom('dino-barn', domString);
+};
+
+
+// Initializer
+const init = () => {
+    buildDinosaurs();
+};
+
+init();
+
+
+
+
+
+
+
+
+
+
+
+// // printToDom function
+// const printToDom = (divId, textToPrint) => {
+//     const ? = document.getElementById(divId);
+//     ?.innerHTML = textToPrint;
+// };
